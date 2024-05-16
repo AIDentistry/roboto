ab0ed2167a55ddeff2a1075c4d3e33ea5e7eb2c0
AIDentistry <zettaverse86@gmail.com>
2024-05-16 11:36:19 +0200

    added api

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "sk-proj-Dht46JKocHIIAF1gvCa2T3BlbkFJ2iv1znlCNKOeJ9qDnXis"; // Paste your API key here
+const API_KEY = "sk-proj-5rGB9NQa6fHYxXoFL3ZfT3BlbkFJHOdSYuAEsEtC5q3x2Vxy"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
