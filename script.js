fa831fe59356f7d5e868071862eebdb88ec10edc
AIDentistry <zettaverse86@gmail.com>
2024-05-24 20:42:13 +0200

    Your commit message

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "sk-proj-XxMP5gMfMOLq87hm5gkdT3BlbkFJ8faz73xwTWzRKSIM2qh3";
+const API_KEY = "";
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
